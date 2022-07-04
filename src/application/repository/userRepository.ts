ce4bf1dd5ed67e7b103efd0d8750a2a59d8739fa
RyanMikhael <62043977+RyanMikhael@users.noreply.github.com>
2022-07-04 16:36:23 -0300

    Delete src directory

--- src/application/repository/userRepository.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../../domain/entities/user";
-
-export interface UserRepository{
-    all(): Promise<User[]>
-    create(obj: User): Promise<String>
-    change(obj: User, id: string): Promise<object>
-    get(id: string): Promise<User>
-}
--- src/domain/entities/user.ts
+++
@@ -1,7 +0,0 @@
-class User{
-    id?: string;
-    name: string;
-    email: string;
-}
-
-export {User};
